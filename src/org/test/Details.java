4e8f21f26df5977b9f63856ec796d99cd78e163a
vidyashree9792 <vshree9792@gmail.com>
2020-04-03 20:57:13 +0530

    My Project Code

---
+++ src/org/test/Details.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class Details {
+	   public static void main(String[] args) {
+	    	 int a=7;
+	    		if (a%2==0) {
+	    			System.out.println("even");
+	    		} else {
+	    	        System.out.println("odd");
+	    		}
+		}
+}
